3090be93d370383e2a394a1bdc881f904a0d86d0
hagward <anders.hagward@gmail.com>
2014-11-22 12:26:17 +0100

    Fixed some errors in opcode decode
    - Missed pc increments
    - Missed index increments

--- src/Chip8.java
+++ src/Chip8.java
@@ -173,18 +173,19 @@ public class Chip8 {
                     case 0x00E0:
                         Arrays.fill(gfx, 0);
                         gfxUpdated = true;
+                        pc += 2;
                         break;
 
                     // 00EE: Returns from subroutine.
                     case 0x00EE:
                         sp--;
                         pc = stack[sp];
+                        pc += 2;
                         break;
 
                     default:
                         System.out.printf("Unknown opcode [0x0000]: 0x%x%n", opCode);
                 }
-                pc += 2;
                 break;
 
             // 1NNN: Jumps to address NNN.
@@ -223,7 +224,8 @@ public class Chip8 {
             // 7XNN: Adds NN to VX.
             case 0x7000:
                 // TODO: is the modulo needed?
-                reg[x] = (reg[x] + (opCode & 0x00FF)) % 256;
+//                reg[x] = (reg[x] + (opCode & 0x00FF)) % 256;
+                reg[x] += opCode & 0x00FF;
                 pc += 2;
                 break;
 
@@ -232,70 +234,85 @@ public class Chip8 {
                     // 8XY0: Sets VX to the value of VY.
                     case 0x0000:
                         reg[x] = reg[y];
+                        pc += 2;
                         break;
 
                     // 8XY1: Sets VX to VX OR VY.
                     case 0x0001:
                         reg[x] |= reg[y];
+                        pc += 2;
                         break;
 
                     // 8XY2: Sets VX to VX AND VY.
                     case 0x0002:
                         reg[x] &= reg[y];
+                        pc += 2;
                         break;
 
                     // 8XY3: Sets VX to VX XOR VY.
                     case 0x0003:
                         reg[x] ^= reg[y];
+                        pc += 2;
                         break;
 
                     // 8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
                     case 0x0004:
-                        reg[15] = (reg[y] > (0xFF - reg[x])) ? 1 : 0;
-                        reg[x] = (reg[x] + reg[y]) % 256;
+                        reg[0xF] = (reg[y] > (0xFF - reg[x])) ? 1 : 0;
+//                        reg[x] = (reg[x] + reg[y]) % 256;
+                        reg[x] += reg[y];
+
+                        assert reg[x] < 256;
+
                         pc += 2;
                         break;
 
                     // 8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
                     case 0x0005:
-                        reg[15] = (reg[y] > reg[x]) ? 0 : 1;
-                        reg[x] = (reg[x] - reg[y]) % 256;
+                        reg[0xF] = (reg[y] > reg[x]) ? 0 : 1;
+//                        reg[x] = (reg[x] - reg[y]) % 256;
+                        reg[x] -= reg[y];
+
                         assert reg[x] >= 0;
+                        assert reg[x] < 256;
+
                         pc += 2;
                         break;
 
                     // 8XY6: Shifts VX right by one. VF is set to the value of the least significant bit of VX before
                     // the shift.
                     case 0x0006:
-                        reg[15] = reg[x] & 1;
+                        reg[0xF] = reg[x] & 1;
                         reg[x] >>= 1;
+                        pc += 2;
                         break;
 
                     // 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
                     case 0x0007:
-                        reg[15] = (reg[x] > reg[y]) ? 0 : 1;
-                        reg[x] = (reg[y] - reg[x]) % 256;
+                        reg[0xF] = (reg[x] > reg[y]) ? 0 : 1;
+//                        reg[x] = (reg[y] - reg[x]) % 256;
+                        reg[x] = reg[y] - reg[x];
+
                         assert reg[x] >= 0;
+
+                        pc += 2;
                         break;
 
                     // 8XYE: Shifts VX left by one. VF is set to the value of the most significant bit of VX before the
                     // shift
                     case 0x000E:
-                        reg[15] = reg[x] >> 7;
+                        reg[0xF] = reg[x] >> 7;
                         reg[x] <<= 1;
+                        pc += 2;
                         break;
 
                     default:
                         System.out.printf("Unknown opcode [0x8000]: 0x%x%n", opCode);
                 }
-                pc += 2;
                 break;
 
             // 9XY0: Skips the next instruction if VX doesn't equal VY.
             case 0x9000:
-                if (reg[x] != reg[y]) {
-                    pc += 4;
-                }
+                pc += (reg[x] != reg[y]) ? 4 : 2;
                 break;
 
             // ANNN: Sets the index register to address NNN.
@@ -311,19 +328,21 @@ public class Chip8 {
 
             // CXNN: Sets VX to a random number and NN.
             case 0xC000:
-                reg[x] = (int) (Math.random() * 10) << 8;
-                reg[x] |= opCode & 0x00FF;
+//                reg[x] = (int) (Math.random() * 10) << 8;
+//                reg[x] |= opCode & 0x00FF;
+                reg[x] = (int) (Math.random() * 256);
+                reg[x] &= opCode & 0x00FF;
                 pc += 2;
                 break;
 
             // DXYN: Sprites stored in memory at location in index register (I), maximum 8 bits wide. Wraps around the
             // screen. If when drawn, clears a pixel, register VF is set to 1 otherwise it is zero. All drawing is XOR
             // drawing (i.e. it toggles the screen pixels).
-            case 0xD000:
+            case 0xD000: {
                 int height = opCode & 0x000F;
                 int pixels;
 
-                reg[15] = 0;
+                reg[0xF] = 0;
                 for (int yLine = 0; yLine < height; yLine++) {
                     // Fetch a row of pixels.
                     pixels = mem[index + yLine];
@@ -331,7 +350,7 @@ public class Chip8 {
                     for (int xLine = 0; xLine < 8; xLine++) {
                         if ((pixels & (128 >> xLine)) != 0) {
                             if (gfx[x + xLine + (y + yLine) * 64] == 1) {
-                                reg[15] = 1;
+                                reg[0xF] = 1;
                             }
                             gfx[x + xLine + (y + yLine) * 64] ^= 1;
                         }
@@ -340,6 +359,7 @@ public class Chip8 {
 
                 gfxUpdated = true;
                 pc += 2;
+            }
                 break;
 
             case 0xE000:
@@ -364,10 +384,11 @@ public class Chip8 {
                     // FX07: Sets VX to the value of the delay timer.
                     case 0x0007:
                         reg[x] = delayTimer;
+                        pc += 2;
                         break;
 
                     // FX0A: A key press is awaited, and then stored in VX.
-                    case 0x000A:
+                    case 0x000A: {
                         boolean keyPress = false;
 
                         for (int i = 0; i < 16; i++) {
@@ -380,27 +401,36 @@ public class Chip8 {
                         if (!keyPress) {
                             return;
                         }
+
+                        pc += 2;
+                    }
                         break;
 
                     // FX15: Sets the delay timer to VX.
                     case 0x0015:
                         delayTimer = reg[x];
+                        pc += 2;
                         break;
 
                     // FX18: Sets the sound timer to VX.
                     case 0x0018:
                         soundTimer = reg[x];
+                        pc += 2;
                         break;
 
                     // FX1E: Adds VX to I.
                     case 0x001E:
-                        index = (index + reg[x]) % 256;
+                        reg[0xF] = (index + reg[x] > 0xFFF) ? 1 : 0;
+//                        index = (index + reg[x]) % 256;
+                        index += reg[x];
+                        pc += 2;
                         break;
 
                     // FX29: Sets I to the location of the sprite for the character in VX. Characters 0-F
                     // (in hexadecimal) are represented by a 4x5 font.
                     case 0x0029:
                         index = reg[x] * 5;
+                        pc += 2;
                         break;
 
                     // FX33: Stores the Binary-coded decimal representation of VX, with the most significant of three
@@ -410,7 +440,9 @@ public class Chip8 {
                     case 0x0033:
                         mem[index] = reg[x] / 100;
                         mem[index + 1] = (reg[x] / 10) % 10;
-                        mem[index + 2] = reg[x] % 10;
+//                        mem[index + 2] = reg[x] % 10;
+                        mem[index + 2] = (reg[x] % 100) % 10;
+                        pc += 2;
                         break;
 
                     // FX55: Stores V0 to VX in memory starting at address I.
@@ -418,6 +450,10 @@ public class Chip8 {
                         for (int i = 0; i < x; i++) {
                             mem[index + i] = reg[i];
                         }
+
+                        // When the operation is done, for some reason, index is set to index + x + 1.
+                        index += x + 1;
+                        pc += 2;
                         break;
 
                     // FX65: Fills V0 to VX with values from memory starting at address I.
@@ -425,12 +461,14 @@ public class Chip8 {
                         for (int i = 0; i < x; i++) {
                             reg[i] = mem[index + i];
                         }
+
+                        index += x + 1;
+                        pc += 2;
                         break;
 
                     default:
                         System.out.printf("Unknown opcode [0xF000]: 0x%x%n", opCode);
                 }
-                pc += 2;
                 break;
 
             default:
