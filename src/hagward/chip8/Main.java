c084270fb361e46beeb4ae7a9ff2c4670e6eba31
hagward <anders.hagward@gmail.com>
2014-11-25 12:53:16 +0100

    Fixed bug where x and y nibbles were read before opCode was set
    Also:
    - Replaced new ActionListener(){} with Java 8 lambda expression.
    - Declared some variables as final.

--- src/hagward/chip8/Main.java
+++ src/hagward/chip8/Main.java
@@ -1,18 +1,15 @@
+package hagward.chip8;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
 
-/**
- * Created by Anders on 2014-11-10.
- */
 public class Main {
 
     public Main() {
         JFrame frame = new JFrame("Hemligt program");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         final Screen screen = new Screen();
         frame.setContentPane(screen);
@@ -22,13 +19,14 @@ public class Main {
         frame.setVisible(true);
 
         final Chip8 chip8 = new Chip8();
-        String fileName = "games/PONG";
+        String fileName = "tests/invaders.c8";
         int fileSize = chip8.loadGame(new File(fileName));
         System.out.printf("Read %s of size %d bytes.%n", fileName, fileSize);
 
         frame.addKeyListener(new KeyListener() {
             @Override
-            public void keyTyped(KeyEvent e) {}
+            public void keyTyped(KeyEvent e) {
+            }
 
             @Override
             public void keyPressed(KeyEvent e) {
@@ -44,14 +42,11 @@ public class Main {
             }
         });
 
-        Timer timer = new Timer(40, new ActionListener() { // should be set to 40
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                chip8.emulateCycle();
-                if (chip8.isGfxUpdated()) {
-                    chip8.drawToImage(screen.getImage());
-                    screen.repaint();
-                }
+        Timer timer = new Timer(40, e -> {
+            chip8.emulateCycle();
+            if (chip8.isGfxUpdated()) {
+                chip8.drawToImage(screen.getImage());
+                screen.repaint();
             }
         });
         timer.start();
--- src/hagward/chip8/Screen.java
+++ src/hagward/chip8/Screen.java
@@ -1,16 +1,14 @@
+package hagward.chip8;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
 
-/**
- * Created by Anders on 2014-11-09.
- */
 public class Screen extends JPanel {
     private static final int WIDTH = 512;
     private static final int HEIGHT = 256;
 
-    private BufferedImage image;
+    private final BufferedImage image;
 
     public Screen() {
         image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -25,6 +23,5 @@ public class Screen extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(image, 0, 0, null);
-        System.out.println("repainted");
     }
 }
